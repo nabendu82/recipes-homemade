f9da658c4e659647c032fc135d36e0b5ed1d4878
nabendu82 <nabendu.biswas@gmail.com>
2020-05-21 19:49:24 +0530

    Part-2 blog complete

---
+++ gatsby-config.js
@@ -0,0 +1,56 @@
+module.exports = {
+  siteMetadata: {
+    title: `Gatsby Default Starter`,
+    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    author: `@gatsbyjs`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: 'gatsby-firesource',
+      options: {
+        credential: require('./firebase.json'),
+        types: [
+          {
+            type: 'Recipe',
+            collection: 'recipes',
+            map: doc => ({
+              name: doc.name,
+              summary: doc.summary,
+              link: doc.link,
+              cook___NODE: doc.cook.id
+            }),
+          },
+          {
+            type: 'Cook',
+            collection: 'cooks',
+            map: doc => ({
+              name: doc.name,
+            }),
+          },
+        ],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+  ],
+}
