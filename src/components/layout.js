5b2e49ccab3027ada705f3deda57ceb4a0040e36
nabendu82 <nabendu.biswas@gmail.com>
2020-05-23 10:43:04 +0530

    Part-5 blog complete

--- src/components/layout.js
+++ src/components/layout.js
@@ -19,20 +19,12 @@ const Layout = ({ children }) => {
   return (
     <>
       <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
         <main>{children}</main>
-        <footer>
+        <footer style={{ textAlign: 'center' }}>
           © {new Date().getFullYear()}, Built by
           {` `}
           <a href="https://thewebdev.tech">Nabendu Biswas</a>
         </footer>
-      </div>
     </>
   )
 }
